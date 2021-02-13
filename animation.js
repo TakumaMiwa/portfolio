6ce3619b707abb4111821df691e3930fb97e862d
TakumaMiwa <takuma.code@gmail.com>
2021-02-13 18:04:34 +0900

    loading_display/completed

--- animation.js
+++ animation.js
@@ -1,4 +1,14 @@
 window.onload = function() {
-    const spinner = document.getElementById('loading');
+    const spinner = document.getElementById('loading_display');
     spinner.classList.add('loaded');
   }
+
+let loading = document.querySelector(".loading_title");
+let letters = loading.textContent.split("");
+loading.textContent = "";
+letters.forEach((letter, i) => {
+  let span = document.createElement("span");
+  span.textContent = letter;
+  span.style.animationDelay = `${i / 5}s`;
+  loading.append(span);
+});
